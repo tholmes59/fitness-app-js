9814b9fc9541bbac0fbbef0dc8103eec3d12a266
Thomas <tholmes59@gmail.com>
2019-05-20 14:30:48 -0700

    add helper methods to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+    rotect_from_forgery with: :exception
+
+    helper_method :current_user
+    helper_method :logged_in?
+    before_action :require_login, except: [:new, :index]
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end 
+
+    def logged_in?
+        !!current_user
+    end 
+
+    def require_login
+        redirect_to root_path unless logged_in?
+    end 
+
 end
