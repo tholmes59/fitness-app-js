fab3d1e2d3f8738f625cebf9f7d9859c5e4f187f
Thomas <tholmes59@gmail.com>
2019-05-20 11:27:06 -0700

    add routes for users, users signup, login, and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'sessions#home'
+  get '/signup', to: 'user#new'
+  post '/signup', to: 'user#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post 'logout', to: 'sessions#destroy'
+
+  get '/auth/facebook/callback', to: 'sessions#create'
+
+  resources :users
+
 end
