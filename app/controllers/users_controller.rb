dd512c5af8594cb64c933abcb3dfc2f10a888ab6
Thomas <tholmes59@gmail.com>
2019-05-20 15:35:27 -0700

    edit redirect paths in create methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id 
-            redirect to root_path
+            redirect to user_path(@user)
         else
             render :new
         end
