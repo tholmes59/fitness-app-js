53fcb7ca81c8cb1086e78c689d93aa47c899e438
Thomas <tholmes59@gmail.com>
2019-05-20 12:11:56 -0700

    add create, show and index methods to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,21 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        
+        if @user.save
+            session[:user_id] = @user.id 
+            redirect to root_path
+        else
+            render :new
+        end
     end
 
+    def show 
+        @user = User.find(params[:id])
+    end
+
+    def index
+        @user = User.all 
+    end
 
     private
 
