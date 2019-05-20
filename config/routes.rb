af0ddb409f67d0381e70a81d92967bc77a5eb5c3
Thomas <tholmes59@gmail.com>
2019-05-20 15:36:36 -0700

    fix spelling for signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'sessions#home'
-  get '/signup', to: 'user#new'
-  post '/signup', to: 'user#create'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
