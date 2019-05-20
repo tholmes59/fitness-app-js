990c2bec3004841dee80856e24bc967d0381b281
Thomas <tholmes59@gmail.com>
2019-05-20 13:52:07 -0700

    add new, create, facebook, destroy, and home methods to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,46 @@
 class SessionsController < ApplicationController
 
+    def new
+        @user = User.new
+        render :login
+      end
+    
+      def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+          session[:user_id] = @user.id
+          redirect_to root_path
+        else
+          flash[:error] = "Sorry, your username or password was incorrect"
+          redirect_to '/login'
+        end
+      end
+    
+      def facebook
+        @user = User.find_or_create_by(uid: auth['uid']) do |u|
+          u.username = auth['info']['name']
+          u.email = auth['info']['email']
+          u.password = auth['uid']
+        end
+       
+        session[:user_id] = @user.id
+    
+        redirect_to root_path
+      end
+    
+      def home
+        
+      end
+    
+      def destroy
+        session.clear
+        redirect_to '/'
+      end
+    
+      private
+    
+      def auth
+        request.env['omniauth.auth']
+      end
+
 end
