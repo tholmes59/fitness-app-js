03d4433fde30fc4be1bde2751bd462a7c02b05ea
Thomas <tholmes59@gmail.com>
2019-05-20 18:15:41 -0700

    edit omniauth route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  get '/auth/facebook/callback', to: 'sessions#create'
+  get '/auth/facebook/callback', to: 'sessions#facebook'
 
   resources :users
 
