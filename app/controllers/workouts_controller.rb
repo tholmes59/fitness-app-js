6d4eed096c87299b41fe89073838ee17d4fbf4a4
Thomas <tholmes59@gmail.com>
2019-05-21 12:43:40 -0700

    create index, show, edit, update, set_workout methods and edit the new and create methods

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,18 +2,44 @@ class WorkoutsController < ApplicationController
 
     def new
         @workout = Workout.new(user_id: params[:user_id])
-
+        @workout.build_multiple_exercises
     end 
 
     def create
         @workout = current_user.workouts.build(workout_params)
-        if workout.save?
+        if @workout.save
             flash[:message] = "Workout created successfully!"
             redirect_to workout_path(@workout)
         else
             render :new
         end
     end
+
+    def index
+        @workouts = Workout.all
+
+    end
+
+    def show
+        set_workout
+    end
+
+    def edit
+        set_workout
+        @workout.build_multiple_exercises
+    end
+
+    def update
+        set_workout
+        if @workout.update(workout_params)
+            flash[:message] = "Workout editied successfully"
+            redirect_to workout_path(@workout)
+        else
+            render :edit
+        end
+    end
+
+
 
 
 
@@ -35,6 +61,13 @@ class WorkoutsController < ApplicationController
                 ]
             ]
         )
+    end
+
+    def set_workout
+        @workout = Workout.find_by(id: params[:id])
+        if !@workout
+            redirect_to workouts_path
+        end
     end
 
 
