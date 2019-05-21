b8ee3783dbf4140a517566d7869f5bfc2e09f606
Thomas <tholmes59@gmail.com>
2019-05-21 15:08:07 -0700

    edit custom method in workout model

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -6,19 +6,11 @@ class Workout < ApplicationRecord
     validates :workout_name, presence: true, uniqueness: true
     validates :workout_description, presence: true
     validates :workout_instructions, presence: true
-    
-    validate :validate_workout_exercises
 
     accepts_nested_attributes_for :workout_exercises, reject_if: proc { |attributes| attributes['sets'].blank? && attributes['repetitions'].blank? && attributes['exercise_attributes']['exercise_name'].blank? }
 
-    def validate_workout_exercises
-        if workout_exercises.length < 1
-          errors.add(:workout_exercises, "You must have at least one set, one repetition, and one exercise")
-        end
-    end
-
     def build_multiple_exercises
-        10.times { workout_exercises.build.build_exercise }
+        7.times { workout_exercises.build.build_exercise }
     end
 
 end
