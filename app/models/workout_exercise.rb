497df6333457abf3c0d0d68de51b997ad01a394f
Thomas <tholmes59@gmail.com>
2019-05-21 15:12:07 -0700

    edit custom method for exercise attributes in workout_exercises model

--- app/models/workout_exercise.rb
+++ app/models/workout_exercise.rb
@@ -3,24 +3,15 @@ class WorkoutExercise < ApplicationRecord
     belongs_to :exercise 
 
     validates :sets, presence: true
-    validates :exercise, presence: true
-    validates :workout, presence: true
-    validate :validate_exercise
+    validates :repetitions, presence: true
+    # validates :exercise, presence: true
+    # validates :workout, presence: true
 
 
-
-    def exercise_attributes=(exercise_attributes)
+     def exercise_attributes=(exercise_attributes)
         exercise_attributes.values.each do |attribute|
           self.exercise = Exercise.find_or_initialize_by(exercise_name: attribute)
         end
     end
-
-    def validate_exercise
-        if exercise.nil? || exercise.name.blank?
-          errors.add(:exercise, "must list one exercise listed")
-        end
-    end
-
-
 
 end
