1d80d15176a19a662bc25675d55c24f398f94609
Thomas <tholmes59@gmail.com>
2019-05-21 12:53:31 -0700

    add validations and custom methods to validate exercise and create exercise attributes in workout_exercise model

--- app/models/workout_exercise.rb
+++ app/models/workout_exercise.rb
@@ -1,6 +1,25 @@
-class ExerciseWorkout < ApplicationRecord
+class WorkoutExercise < ApplicationRecord
     belongs_to :workout 
     belongs_to :exercise 
+
+    validates :sets, presence: true
+    validates :exercise, presence: true
+    validates :workout, presence: true
+    validate :validate_exercise
+
+
+
+    def exercise_attributes=(exercise_attributes)
+        exercise_attributes.values.each do |attribute|
+          self.exercise = Exercise.find_or_initialize_by(exercise_name: attribute)
+        end
+    end
+
+    def validate_exercise
+        if exercise.nil? || exercise.name.blank?
+          errors.add(:exercise, "must list one exercise listed")
+        end
+    end
 
 
 
