eaffd63e2af50f50b871e1438f623d10adb07e73
Thomas <tholmes59@gmail.com>
2019-05-21 12:48:22 -0700

    add association to workouts in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
+    has_many :workouts
 
-
-has_secure_password
-validates :username, :email, presence: true
-validates :username, :email, uniqueness: true
+    has_secure_password
+    validates :username, :email, presence: true
+    validates :username, :email, uniqueness: true
 
 end
