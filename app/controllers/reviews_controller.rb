92e5b56fe247f6992641a56250224a45e3b406a4
Thomas <tholmes59@gmail.com>
2019-05-22 12:38:46 -0700

    create reviews_controller and add CRUD methods for reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,47 @@
+class ReviewsController < ApplicationController
+
+    def new
+        @workout = Workout.find(params[:workout_id])
+        @review = Review.new
+      end
+    
+      def create
+        @workout = Workout.find(params[:workout_id])
+        @review = @workout.reviews.build(review_params)
+        @review.user = current_user
+        if @review.save
+          flash[:message] = "Sucessfully added review!"
+          redirect_to workout_reviews_path
+        else
+          render :new
+        end
+      end
+    
+      def index
+        # if params[:workout_id]
+        #   @reviews = Workout.find(params[:workout_id]).reviews.most_recent
+        # else
+        #   @reviews = Review.all.most_recent
+        # end
+        @workout = Workout.find_by_id(params[:workout_id])
+            if @workout
+               @reviews = @workout.reviews.most_recent
+            else
+               redirect_to workouts_path
+            end
+        end
+    
+      def show
+        @review = Review.find(params[:id])
+      end
+    
+      private
+    
+      def review_params
+        params.require(:review).permit(:content, :rating, :workout_id)
+      end
+
+      
+
+
+end
