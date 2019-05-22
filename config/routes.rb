8a5e066b72c8f7f5ee4c0c54b34bd52e8b623ba4
Thomas <tholmes59@gmail.com>
2019-05-21 21:09:43 -0700

    edit routes to add welcome, static and nested users/workout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'sessions#home'
+  
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
@@ -8,9 +8,17 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
+  
+
   get '/auth/facebook/callback', to: 'sessions#facebook'
 
   resources :users
   resources :workouts
 
+  resources :users, only: [:show] do
+    resources :workouts, only: [:index]
+  end
+
+  root 'static#home'
+  get '/home', to: 'welcome#home'
 end
