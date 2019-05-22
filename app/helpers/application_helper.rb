df8c399c8161e1cdb2fd6843898074dbf4101504
Thomas <tholmes59@gmail.com>
2019-05-22 12:45:14 -0700

    add helper method to display created at date for reviews

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def review_count_label(reviews)
+        if reviews.count == 0
+          "This workout has no reviews."
+        elsif reviews.count == 1
+          "1 review"
+        else
+          "#{reviews.count} reviews"
+        end
+    end
 end
+
