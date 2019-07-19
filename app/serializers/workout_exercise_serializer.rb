2617c6ef883bb401c1a7bcd1b43b0c569c61e56e
Thomas <tholmes59@gmail.com>
2019-07-19 14:30:39 -0700

    add attributes and associations to workout_exercises serializer

--- app/serializers/workout_exercise_serializer.rb
+++ app/serializers/workout_exercise_serializer.rb
@@ -1,3 +1,7 @@
 class WorkoutExerciseSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :workout_id, :exercise_id, :sets, :repetitions
+
+  belongs_to :workout 
+  belongs_to :exercise
+   
 end
