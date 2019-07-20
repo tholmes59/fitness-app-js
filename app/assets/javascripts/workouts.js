d62c2bebcc6ec0b052a6ffad2fb46012423f0678
Thomas <tholmes59@gmail.com>
2019-07-20 11:42:09 -0700

    continue to build bindClickHandlers function

--- app/assets/javascripts/workouts.js
+++ app/assets/javascripts/workouts.js
@@ -6,5 +6,7 @@ const bindClickHandlers = () => {
     $('#all-workouts').on('click', (e) => {
         e.preventDefault()
         fetch('/workouts.json')
+        .then(res => res.json())
+        .then(data => console.log(data))
     })
 }
