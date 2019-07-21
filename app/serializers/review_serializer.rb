f866d4b1bc5a37f13f30a68d6dedf07a4295d1fc
Thomas <tholmes59@gmail.com>
2019-07-21 08:31:25 -0700

    add created_at attributes to serializers

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :content, :rating, :workout_id, :user_id
+  attributes :id, :content, :rating, :workout_id, :user_id, :created_at
 
   belongs_to :user
   belongs_to :workout
--- app/serializers/workout_serializer.rb
+++ app/serializers/workout_serializer.rb
@@ -1,9 +1,9 @@
 class WorkoutSerializer < ActiveModel::Serializer
-  attributes :id, :workout_name, :workout_description, :workout_instructions, :user_id
+  attributes :id, :workout_name, :workout_description, :workout_instructions, :user_id, :created_at
 
   belongs_to :user
   has_many :workout_exercises
   has_many :exercises, through: :workout_exercises
   has_many :reviews
-  
+
 end
