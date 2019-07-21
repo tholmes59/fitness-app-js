2fb478a19e5597567e5c4a3f329ee6819856f3be
Thomas <tholmes59@gmail.com>
2019-07-21 08:02:34 -0700

    add constructor function for workouts

--- app/assets/javascripts/workouts.js
+++ app/assets/javascripts/workouts.js
@@ -10,3 +10,12 @@ const bindClickHandlers = () => {
         .then(data => console.log(data))
     })
 }
+
+
+function Workout(workout) {
+    this.id = workout.id;
+    this.workout_name = workout.workout_name;
+    this.workout_description = workout.workout_description;
+    this.workout_instructions = workout.workout_instructions;
+    this.created_at = workout.created_at;
+}
