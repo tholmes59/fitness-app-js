c424a590ca61285bd8ccacbefe5cb0d2f1e1f407
Thomas <tholmes59@gmail.com>
2019-07-23 10:04:23 -0700

    edit exercise constructor function

--- app/assets/javascripts/exercise.js
+++ app/assets/javascripts/exercise.js
@@ -1,5 +1,4 @@
 function Exercise(exercise) {
+    this.id = exercise.id
     this.exercise_name = exercise.exercise_name;
-    this.sets = exercise.sets;
-    this.repetitions = exercise.repetitions;
 }
