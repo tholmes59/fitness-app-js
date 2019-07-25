f31f899260d8882399e955830e85b20dbac4d96f
Thomas <tholmes59@gmail.com>
2019-07-25 11:20:54 -0700

    add exercise name to workout exercises constructor function

--- app/assets/javascripts/workout_exercise.js
+++ app/assets/javascripts/workout_exercise.js
@@ -2,4 +2,5 @@ function WorkoutExercise(workout_exercise) {
     this.id = workout_exercise.id;
     this.sets = workout_exercise.sets;
     this.repetitions = workout_exercise.repetitions;
+    this.exercise_name = workout_exercise.exercise_name;
 }
