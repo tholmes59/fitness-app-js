82e7ffba4c6279b4a21a118b9681becfcedd3537
Thomas <tholmes59@gmail.com>
2019-07-25 11:17:17 -0700

    edit serializer to include exercise name

--- app/serializers/workout_exercise_serializer.rb
+++ app/serializers/workout_exercise_serializer.rb
@@ -1,7 +1,17 @@
 class WorkoutExerciseSerializer < ActiveModel::Serializer
-  attributes :id, :workout_id, :exercise_id, :sets, :repetitions
+  attributes :id, :workout_id, :exercise_id, :sets, :repetitions, :created_at, :updated_at
 
   belongs_to :workout 
-  belongs_to :exercise
+  belongs_to :exercise, serializer: ExerciseSerializer
+
+  # accepts_nested_attributes_for :exercise
+
+  # def exercise_attributes=(exercise_attributes)
+  #   exercise_attributes.values.each do |attribute|
+  #     self.exercise = Exercise.find_or_initialize_by(exercise_name: attribute)
+  #   end
+  # end
+
+  attribute(:exercise_name) {|i| i.object.exercise.exercise_name}
   
 end
