c36a64ef2bf5dc8da1050f585711178d22b366a7
Thomas <tholmes59@gmail.com>
2019-08-01 09:16:22 -0700

    remove unused code in serializer

--- app/serializers/workout_exercise_serializer.rb
+++ app/serializers/workout_exercise_serializer.rb
@@ -2,15 +2,7 @@ class WorkoutExerciseSerializer < ActiveModel::Serializer
   attributes :id, :workout_id, :exercise_id, :sets, :repetitions, :created_at, :updated_at
 
   belongs_to :workout 
-  belongs_to :exercise, serializer: ExerciseSerializer
-
-  # accepts_nested_attributes_for :exercise
-
-  # def exercise_attributes=(exercise_attributes)
-  #   exercise_attributes.values.each do |attribute|
-  #     self.exercise = Exercise.find_or_initialize_by(exercise_name: attribute)
-  #   end
-  # end
+  belongs_to :exercise
 
   attribute(:exercise_name) {|i| i.object.exercise.exercise_name}
   
