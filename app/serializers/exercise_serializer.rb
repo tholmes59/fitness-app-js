9ba966a4a5bd8ad4885952a1dcf770d0d2bb5bc6
Thomas <tholmes59@gmail.com>
2019-08-01 09:08:12 -0700

    delete unused code in serializer

--- app/serializers/exercise_serializer.rb
+++ app/serializers/exercise_serializer.rb
@@ -4,10 +4,4 @@ class ExerciseSerializer < ActiveModel::Serializer
   has_many :workout_exercises, serializer: WorkoutExerciseSerializer
   has_many :workouts, through: :workout_exercises
 
-  def workout_exercises
-    object.workout_exercises
-  end
-
-  # attribute(:sets) {|i| i.object.workout_exercise.sets}
-
 end
